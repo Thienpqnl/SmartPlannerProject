d430ffea2a150f8e8191bc97d5b1d9fe7e2f1ebe
unknown <22130261@st.hcmuaf.edu.vn>
2025-06-19 16:35:13 +0700

    pull

--- app/src/main/java/com/thien/smart_planner_project/network/RetrofitClient.java
+++ app/src/main/java/com/thien/smart_planner_project/network/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit = null;
-    private static String baseUrl = "http://192.168.1.70:3000";
+    private static String baseUrl = "http://10.0.0.2:3000/";
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
